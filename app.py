d3c68066d76af741508cb88b628891d93b360bf8
Sriharan <sriharan029@gmail.com>
2024-10-03 11:22:10 +0530

    final commit

--- app.py
+++ app.py
@@ -105,5 +105,6 @@ def favicon():
     return '', 204
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+
